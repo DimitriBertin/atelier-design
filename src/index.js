8feedd86bdfe6589f8517c10ba29c32121b3d891
Bertin <dim.bertin@gmail.com>
2022-05-18 12:42:06 +0200

    Start some dynamic tools

--- src/index.js
+++ src/index.js
@@ -2,6 +2,19 @@ import 'swiper/css'
 import './styles/index.scss'
 import Swiper, { Navigation } from 'swiper'
 
+// STICKY ----------
+const header = document.querySelector('header')
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > window.innerHeight / 2) {
+    console.log('OK fixed')
+    header.classList.add('is-fixed')
+  } else {
+    header.classList.remove('is-fixed')
+  }
+})
+
+// SLIDER -------------
 const swiper = new Swiper('[js-slider]', {
   slidesPerView: 1,
   loop: true,
@@ -11,3 +24,10 @@ const swiper = new Swiper('[js-slider]', {
     prevEl: '[js-btn-prev]',
   },
 })
+
+// APPARITION
+const blocks = document.querySelectorAll('[js-scroll-animation]')
+
+// Menu
+const menuButton = document.querySelector('[js-menu-button]')
+const menu = document.querySelector('[js-menu]')
