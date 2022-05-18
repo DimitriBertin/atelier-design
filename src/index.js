163bdfd4c21647c62d135e4d26880cea3ac84baf
Bertin <dim.bertin@gmail.com>
2022-05-18 14:53:57 +0200

    Add some Parallax + scroll animation

--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import Swiper, { Navigation } from 'swiper'
 
 // STICKY ----------
 const header = document.querySelector('header')
+const image = document.querySelector('[js-parallax]')
 
 window.addEventListener('scroll', () => {
   if (window.scrollY > window.innerHeight / 2) {
-    console.log('OK fixed')
     header.classList.add('is-fixed')
   } else {
     header.classList.remove('is-fixed')
   }
+
+  // Parallax
+  image.style.transform = `translateY(${window.scrollY * 0.05}px)`
 })
 
 // SLIDER -------------
@@ -28,6 +31,25 @@ const swiper = new Swiper('[js-slider]', {
 // APPARITION
 const blocks = document.querySelectorAll('[js-scroll-animation]')
 
+var options = {
+  root: document,
+  rootMargin: '0px',
+  threshold: 0.25
+}
+
+for (const block of blocks) {
+  const observer = new IntersectionObserver((entries, observer) => {
+    if (entries[0].isIntersecting) {
+      block.classList.add('is-appear')
+      observer.disconnect()
+    }
+  },options)
+  observer.observe(block)
+}
+
+
 // Menu
 const menuButton = document.querySelector('[js-menu-button]')
 const menu = document.querySelector('[js-menu]')
+
+// PARALLAX
